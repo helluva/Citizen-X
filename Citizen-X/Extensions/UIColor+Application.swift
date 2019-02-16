b6d60674227b9fa6b8221e54156dd0858679ad4b
cliffpanos <cliffpanos@gmail.com>
2019-02-16 11:54:27 -0800

    Add colors, app icon prototype, storyboard configuration

---
+++ Citizen-X/Extensions/UIColor+Application.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+Application.swift
+//  Citizen-X
+//
+//  Created by Cliff Panos on 2/16/19.
+//  Copyright © 2019 Clifford Panos. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class var applicationPrimary: UIColor {
+        return UIColor(named: "Primary")
+    }
+    
+    class var applicationSecondary: UIColor {
+        return UIColor(named: "Secondary")
+    }
+    
+}
