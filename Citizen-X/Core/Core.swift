8aab28fb0ee5805c77a516801fedb7fe5da5ff5d
cliffpanos <cliffpanos@gmail.com>
2019-02-16 10:00:42 -0800

    Update project structure and add Phone2Action API key

---
+++ Citizen-X/Core/Core.swift
@@ -0,0 +1,12 @@
+//
+//  Core.swift
+//  Citizen-X
+//
+//  Created by Cliff Panos on 2/16/19.
+//  Copyright © 2019 Clifford Panos. All rights reserved.
+//
+
+import Foundation
+
+/// The API key for accessing 
+let CXAppPhone2ActionAPIKey = "2e1uvo7yeX50ZGHvctPxi8ZWubhggyOydIWvOa5c"
