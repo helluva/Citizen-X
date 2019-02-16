b36cc47d84cf6d6d1601ee69df5ffd205c0ec0aa
cliffpanos <cliffpanos@gmail.com>
2019-02-16 10:20:39 -0800

    Add ModernUIKit.framework. Target iOS 11.0

---
+++ ModernUIKit/ModernUIKit.h
@@ -0,0 +1,19 @@
+//
+//  ModernUIKit.h
+//  ModernUIKit
+//
+//  Created by Cliff Panos on 2/16/19.
+//  Copyright © 2019 Clifford Panos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModernUIKit.
+FOUNDATION_EXPORT double ModernUIKitVersionNumber;
+
+//! Project version string for ModernUIKit.
+FOUNDATION_EXPORT const unsigned char ModernUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModernUIKit/PublicHeader.h>
+
+
