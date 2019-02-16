671b09f29e293c27dfedc9852af32eb33313169c
cliffpanos <cliffpanos@gmail.com>
2019-02-16 09:42:28 -0800

    Create Xcode Project and add gitignore

---
+++ Citizen-X/Citizen-X/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Citizen-X
+//
+//  Created by Cliff Panos on 2/16/19.
+//  Copyright © 2019 Clifford Panos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
