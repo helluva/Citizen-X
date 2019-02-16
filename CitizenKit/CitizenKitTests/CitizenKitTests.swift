5d6a5489c667a0268da720f30910a519baea5444
Cal Stephens <cal@calstephens.tech>
2019-02-16 10:11:30 -0800

    Add CitizenKit scaffolding

---
+++ CitizenKit/CitizenKitTests/CitizenKitTests.swift
@@ -0,0 +1,13 @@
+//
+//  CitizenKitTests.swift
+//  CitizenKitTests
+//
+//  Created by Cal Stephens on 2/16/19.
+//  Copyright © 2019 Clifford Panos. All rights reserved.
+//
+
+import XCTest
+
+class CitizenKitTests: XCTestCase {
+
+}
